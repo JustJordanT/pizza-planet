bac9ab82b4ee2a81c95903b028c6f2be368955f3
Jordan <Jordantay9014@outlook.com>
2023-01-14 13:39:27 -0600

    overhual... semi good working point

---
+++ PizzaPlanet.API/Migrations/20230113224815_initialv1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaPlanet.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialv1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    CustomerId = table.Column<string>(type: "text", nullable: false),
+                    CustomerId1 = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartEntity_CustomerEntity_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CustomerEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartEntity_CustomerEntity_CustomerId1",
+                        column: x => x.CustomerId1,
+                        principalTable: "CustomerEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartEntity_CustomerId",
+                table: "CartEntity",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartEntity_CustomerId1",
+                table: "CartEntity",
+                column: "CustomerId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartEntity");
+
+            migrationBuilder.DropTable(
+                name: "CustomerEntity");
+        }
+    }
+}
---
+++ PizzaPlanet.API/Migrations/20230113225807_initialv2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaPlanet.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartEntity_CustomerEntity_CustomerId1",
+                table: "CartEntity");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartEntity_CustomerId1",
+                table: "CartEntity");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId1",
+                table: "CartEntity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerId1",
+                table: "CartEntity",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartEntity_CustomerId1",
+                table: "CartEntity",
+                column: "CustomerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartEntity_CustomerEntity_CustomerId1",
+                table: "CartEntity",
+                column: "CustomerId1",
+                principalTable: "CustomerEntity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
