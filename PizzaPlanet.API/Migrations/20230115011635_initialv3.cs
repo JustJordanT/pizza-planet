87eeea2e846776f387026105674ada9d803a7343
Jordan <Jordantay9014@outlook.com>
2023-01-14 20:58:59 -0600

    working customer/cart/order

---
+++ PizzaPlanet.API/Migrations/20230115011635_initialv3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaPlanet.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialv3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    CartId = table.Column<string>(type: "text", nullable: false),
+                    OrderStatus = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderEntity_CartEntity_CartId",
+                        column: x => x.CartId,
+                        principalTable: "CartEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderEntity_CartId",
+                table: "OrderEntity",
+                column: "CartId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderEntity");
+        }
+    }
+}
