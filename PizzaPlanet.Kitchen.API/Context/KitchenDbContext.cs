b0ab789b4771fafff52c5d9ae3ff48ce94ab0fad
Jordan <Jordantay9014@outlook.com>
2023-01-18 16:36:08 -0600

    starting on kitchen

--- PizzaPlanet.Kitchen.API/Context/KitchenDbContext.cs
+++ PizzaPlanet.Kitchen.API/Context/KitchenDbContext.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace PizzaPlanet.Kitchen.API.Context;
 
-public class KitchenDbContext
+public class KitchenDbContext : DbContext 
 {
-    
+    // public DbSet<Cooks> Cook { get; set; }
+    // public DbSet<Cooks> Cook { get; set; }
 }
